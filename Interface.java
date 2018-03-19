ddba81938203280ddf69ffc76f47f732b0346980
JackHardy99 <37541773+JackHardy99@users.noreply.github.com>
2018-03-19 13:00:14 +0000

    Add files via upload
    Adds functionality to the buttons so the images move plus validation so they move correctly

--- Interface.java
+++ Interface.java
@@ -58,3 +58,237 @@ public class GUI implements ActionListener {
 		}
 
 	}
+	public void actionPerformed(ActionEvent e)
+	{
+			
+			ImageIcon blank = new ImageIcon("bart0.jpg");
+			Icon temp = new ImageIcon();
+	
+			if(e.getSource() == buttons[0][0]){
+				XValue = 0;
+				YValue = 0;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+					
+						
+						temp = buttons[XValue][YValue].getIcon() ;	//Stores the image of the button you click on
+						buttons[XValue][YValue].setIcon(blank);		//Sets the button's image that you click on to blank
+						buttons[lastXValue][lastYValue].setIcon(temp);	//Sets the image of the button that was blank to the temp image
+			
+						lastXValue = 0;
+						lastYValue = 0;
+				
+					
+				}
+				Leaderboard();
+			}
+				
+			else if (e.getSource() == buttons[0][1]){
+				XValue = 0;
+				YValue = 1;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;	
+
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+						temp = buttons[XValue][YValue].getIcon() ;						
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+						lastXValue = 0;
+						lastYValue = 1;
+					
+				}
+				Leaderboard();
+			}			
+			
+			else if (e.getSource() == buttons[0][2]){
+				XValue = 0;
+				YValue = 2;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;
+				
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+			
+						temp = buttons[XValue][YValue].getIcon() ;
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+			
+						lastXValue = 0;
+						lastYValue = 2;
+					
+				}
+				Leaderboard();
+			}
+			else if (e.getSource() == buttons[0][3]){
+				XValue = 0;
+				YValue = 3;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;		
+				
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+						temp = buttons[XValue][YValue].getIcon() ;
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+	
+						lastXValue = 0;
+						lastYValue = 3;
+					
+
+				}
+				Leaderboard();
+			}
+
+			else if(e.getSource() == buttons[1][0]){
+				XValue = 1;
+				YValue = 0;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;
+			
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+				
+						temp = buttons[XValue][YValue].getIcon();
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+
+						lastXValue = 1;
+						lastYValue = 0;
+					
+
+				}
+				Leaderboard();
+			}
+
+			else if(e.getSource() == buttons[1][1]){
+				XValue = 1;
+				YValue = 1;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;
+				
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+
+						temp = buttons[XValue][YValue].getIcon() ;
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+
+						lastXValue = 1;
+						lastYValue = 1;
+					
+				}
+				Leaderboard();
+			}
+				
+
+			else if(e.getSource() == buttons[1][2]){
+				XValue = 1;
+				YValue = 2;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;
+				
+
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+						temp = buttons[XValue][YValue].getIcon() ;
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+	
+						lastXValue = 1;
+						lastYValue = 2;
+					
+
+				}
+				Leaderboard();
+			}	
+
+			else if(e.getSource() == buttons[1][3]){
+				XValue = 1;
+				YValue = 3;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;
+				
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+						temp = buttons[XValue][YValue].getIcon() ;
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+
+						lastXValue = 1;
+						lastYValue = 3;
+					
+				}
+				Leaderboard();
+			}
+				
+			else if(e.getSource() == buttons[2][0]){
+				XValue = 2;
+				YValue = 0;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;
+				
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+						temp = buttons[XValue][YValue].getIcon() ;
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+
+						lastXValue = 2;
+						lastYValue = 0;	
+					
+				}
+				Leaderboard();
+			}
+				
+			else if(e.getSource() == buttons[2][1]){
+				XValue = 2;
+				YValue = 1;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;
+				
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){	
+						temp = buttons[XValue][YValue].getIcon() ;
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+
+						lastXValue = 2;
+						lastYValue = 1;
+					
+
+				}
+				Leaderboard();
+			}
+	
+			else if(e.getSource() == buttons[2][2]){
+				XValue = 2;
+				YValue = 2;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;
+
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+						temp = buttons[XValue][YValue].getIcon() ;
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+
+						lastXValue = 2;
+						lastYValue = 2;
+					
+				}
+				Leaderboard();
+			}
+
+		
+			else if(e.getSource() == buttons[2][3]){
+				XValue = 2;
+				YValue = 3;
+				testX = XValue - lastXValue;
+				testY = YValue - lastYValue;
+				
+				if((testX == 1 && testY == 0) || (testX == -1 && testY == 0) || (testX == 0 && testY == 1) || (testX == 0 && testY == -1)){
+						temp = buttons[XValue][YValue].getIcon() ;
+						buttons[XValue][YValue].setIcon(blank);
+						buttons[lastXValue][lastYValue].setIcon(temp);
+
+						lastXValue = 2;
+						lastYValue = 3;
+					
+
+				}
+				Leaderboard();
+			}
+			
+	}
