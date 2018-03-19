e5e1d69987073ed61e47360336c6b00cb4341ead
JackHardy99 <37541773+JackHardy99@users.noreply.github.com>
2018-03-19 17:32:52 +0000

    Add files via upload
    Added more javadoc

--- Puzzle.java
+++ Puzzle.java
@@ -1,6 +1,7 @@
 /**
 *<h1>Simpsons Picture Puzzle</h1>
 *This Picture Puzzle uses a Simpsons image, split into 12 parts with one blank.
+*<p>
 *The aims is to get the 12 parts back into the original order by moving images adjacent to the blank
 *picture with the blank picture
 *
@@ -9,7 +10,11 @@
 *@since 2018-03-17
 */
 public class Puzzle {
-
+	/**
+	*This class is the instanciator for the entire program, to run the puzzle this is the class
+	*that does so.
+	*
+	*/
 	public static void main(String[] args) {
 
 		Interface gameBoard = new Interface();
