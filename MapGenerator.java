c561a5f3df6e99aed7a33fbc16f3b1238b1bc63e
JackHardy99 <37541773+JackHardy99@users.noreply.github.com>
2018-05-23 00:41:29 +0100

    Health of Bricks
    Adds health of bricks to each brick and adds potential to move bricks down

--- MapGenerator.java
+++ MapGenerator.java
@@ -6,6 +6,8 @@ public class MapGenerator {
 	public int map[][];
 	public int brickWidth;
 	public int brickHeight;
+	public int numberOfDrops =1;
+	public boolean bricksBottom = false;
 	
 	
 	public MapGenerator (int row,int col, int brickHealth) {
@@ -20,25 +22,29 @@ public class MapGenerator {
 		brickWidth = 540/7;
 		brickHeight = 150/3;
 	}
+	
 	public void draw(Graphics2D g) {
 		for(int i=-0;i<map.length;i++) {
 			for(int j = 0;j<map[0].length;j++) {
 				if(map[i][j] >0) {
-						
+						if (i*brickHeight + (numberOfDrops *50) > 924) {
+							bricksBottom = true;
+						}
+						else {
 							g.setColor(Color.WHITE);
-							g.fillRect(j*brickWidth +80, i*brickHeight + 50, brickWidth, brickHeight);
+							g.fillRect(j*brickWidth +80, i*brickHeight +(numberOfDrops * 50), brickWidth, brickHeight);
 							g.setColor(Color.red);
-							
+							g.drawString(""+map[i][j], 80+(brickWidth*(j+1))-(brickWidth/2)-(3*j), (i+1)*(brickHeight)-(brickHeight/2) +(numberOfDrops*50)+(3*i));
 						
 							g.setStroke(new BasicStroke(3));
 							g.setColor(Color.black);
 							
-							g.drawRect(j*brickWidth +80, i*brickHeight + 50, brickWidth, brickHeight);
+							g.drawRect(j*brickWidth +80, i*brickHeight +(numberOfDrops * 50), brickWidth, brickHeight);
 						}
 				}
 			}
 		}
-	
+	}
 	
 	public void setBrickValue(int row, int col) {
 		map[row][col] = map[row][col] -1;
@@ -47,3 +53,4 @@ public class MapGenerator {
 		return map[row][col];
 	}
 }
+
