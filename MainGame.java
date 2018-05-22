d6449d12fbea4172c220d9d7f580330ddf2f5383
JackHardy99 <37541773+JackHardy99@users.noreply.github.com>
2018-05-22 22:45:55 +0100

    Add main method and adjusted the frame and added Gameplay

--- MainGame.java
+++ MainGame.java
@@ -1,5 +1,13 @@
 import javax.swing.*;
 public class MainGame {
-	JFrame gameFrame = new JFrame();
-	Gameplay gameplay = new Gameplay();
+	public static void main(String[] args)  {
+		JFrame gameFrame = new JFrame();
+		Gameplay gameplay = new Gameplay();
+	
+		gameFrame.setSize(700,1000);
+		gameFrame.setTitle("Brick Busters");
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameFrame.add(gameplay);
+		gameFrame.setVisible(true);
+	}
 }
