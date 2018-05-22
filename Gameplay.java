11a329d7bd800198d814c40cac0a81c8e73b9de1
JackHardy99 <37541773+JackHardy99@users.noreply.github.com>
2018-05-22 22:48:53 +0100

    Imported javax and added in a timer

--- Gameplay.java
+++ Gameplay.java
@@ -1,4 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Gameplay {
-
+	private Timer timer;
+	private int delay = 8;
+	public Gameplay() {
+		
+		timer = new Timer(delay, this);
+		timer.start();
+		
+	}
 }
