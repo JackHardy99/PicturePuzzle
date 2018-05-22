52a37790560e0ff1bc4cd5bcf53ed1c8715c05ea
JackHardy99 <37541773+JackHardy99@users.noreply.github.com>
2018-05-23 00:21:36 +0100

    Creates all the bricks in an array
    2D array to store all of the bricks and their health in.

--- MapGenerator.java
+++ MapGenerator.java
@@ -1,4 +1,20 @@
 
 public class MapGenerator {
-
+	public int map[][];
+	public int brickWidth;
+	public int brickHeight;
+	
+	
+	public MapGenerator (int row,int col, int brickHealth) {
+		map = new int[row][col];
+		for(int i=-0;i<map.length;i++) {
+			for(int j = 0;j<map[0].length;j++) {
+				map[i][j]=brickHealth; //the one shows the brick has not intercepted the ball
+							//can change this for bricks to have more health
+			}
+		}
+		
+		brickWidth = 540/7;
+		brickHeight = 150/3;
+	}
 }
