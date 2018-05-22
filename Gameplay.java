3402e357e296aa34e28d7776a7429d8b9fa12f7f
JackHardy99 <37541773+JackHardy99@users.noreply.github.com>
2018-05-22 22:58:49 +0100

    Paint: Launcher and Ball

--- Gameplay.java
+++ Gameplay.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 
 public class Gameplay extends JPanel implements MouseListener, KeyListener, ActionListener{
 	private Timer timer;
-	private int delay = 8;
+	private int delay = 8, ballPosX = 340, ballPosY = 925;
 	public Gameplay() {
 		
 		timer = new Timer(delay, this);
@@ -21,6 +21,14 @@ public class Gameplay extends JPanel implements MouseListener, KeyListener, Acti
 		g.fillRect(0, 0, 3, 1000);
 		g.fillRect(0, 0, 700, 3);
 		g.fillRect(697, 0, 3, 1000);
+		
+		//launcher
+		g.setColor(Color.white);
+		g.fillRect(340, 950, 20, 50);
+
+		//ball
+		g.setColor(Color.RED);
+		g.fillOval(ballPosX, ballPosY, 20, 20);
 		
 		g.dispose();
 	}
